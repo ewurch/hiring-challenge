7da04ef4d0e5d42cfcbbcc5c93300dc008e73d8f
Eduardo Würch <ewurch@gmail.com>
2022-11-09 20:25:05 -0300

    refactor api module

--- src/api/main.py
+++ src/api/main.py
@@ -1,41 +1,53 @@
 from fastapi import FastAPI, HTTPException
 
-from cluster.core import cluster, Group, find_group
+from cluster.core import Cluster, Node, Group, find_group
 
 app = FastAPI(
     title="Cluster Management API",
     version="0.1.0"
 )
 
+# create connection to cluster
+cluster = Cluster()
+
 v1 = FastAPI()
 
-@v1.get("/", tags=["Cluster"])
+@v1.get("/")
 def root():
     return cluster
 
-@v1.get("/group/{group_id}", status_code=200, tags=["Cluster"])
+@v1.post("/node", status_code=201)
+def add_node(node: Node):
+    cluster.add_node(node)
+    return node
+
+@v1.delete("/node")
+def remove_node(node: Node):
+    cluster.remove_node(node)
+    return node
+
+@v1.get("/group/{group_id}")
 def get_group(group_id: str):
-    """
-    """
     try:
         return find_group(cluster, group_id=group_id)
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
 
-@v1.delete("/group", status_code=200, tags=["Cluster"])
-def delete_group(group: Group):
+@v1.post("/group", status_code=201)
+def create_group(group: Group):
     try:
-        group = cluster.delete_group_from_cluster(group=group)
+        group = cluster.add_group(group=group) 
         return group
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-@v1.post("/group", status_code=201, tags=["Cluster"])
-def create_group(group: Group):
+@v1.delete("/group")
+def remove_group(group: Group):
     try:
-        group = cluster.add_group_to_cluster(group=group) 
+        group = cluster.remove_group(group=group)
         return group
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+
 
 app.mount("/v1", v1)
