b908b5b02f063358606d864c3f3730a2d1c38339
Eduardo Würch <ewurch@gmail.com>
2022-11-09 20:25:23 -0300

    refactor cluster module

--- src/cluster/core.py
+++ src/cluster/core.py
@@ -1,52 +1,105 @@
+from itertools import chain
 from random import choice
-from pydantic import BaseModel, conlist
+from typing import Callable
+from pydantic import BaseModel, Field, validator
 
 class Group(BaseModel):
-    groupID: str
+    groupID: str = Field(..., min_length=1)
 
 class Node(BaseModel):
-    url: str
+    url: str = Field(..., min_length=1)
     groups: list[Group] = []
 
-    def add_group_to_node(self, group: Group):
+    @validator('groups')
+    def unique_group_ids(cls, groups: list[Group]) -> list[Group]:
+        if len(groups) != len(set([group.groupID for group in groups])):
+            raise Exception("Group IDs must be unique")
+        return groups
+
+    def add_group(self, group: Group) -> Group:
         if group not in self.groups:
             self.groups.append(group)
+            return group
         else:
             raise Exception("group already exists")
 
-    def delete_group_from_node(self, group: Group):
+    def remove_group(self, group: Group) -> Group:
         if group in self.groups:
             self.groups.remove(group)
+            return group
         else:
             raise Exception("Group does not exist in node")
 
+    def get_group_ids(self) -> list[str]:
+        return [group.groupID for group in self.groups]
+
 class Cluster(BaseModel):
     nodes: list[Node] = []
+    
+    @validator('nodes')
+    def unique_node_urls(cls, nodes: list[Node]) -> list[Node]:
+        if len(nodes) != len(set([node.url for node in nodes])):
+            raise Exception("Node URLs must be unique")
+        return nodes
 
-    def validate_cluster_consisitency(self) -> bool:
+    def validate_consistency(self) -> bool:
         # Check if all nodes have the same groups
         for node in self.nodes:
             if node.groups != self.nodes[0].groups:
                 return False
         return True
 
-    def rollback_cluster(self) -> None:
+    def rollback(self) -> None:
         # Rollback all nodes to commom state
-        pass 
-
-    def add_node(self, url: str) -> None:
-        self.nodes.append(Node(url=url, groups=[]))
-
-    def delete_group_from_cluster(self, group: Group):
+        n_nodes = len(self.nodes)
+        all_groups = list(chain(*[node.groups for node in self.nodes]))
+        print(self.nodes)
         for node in self.nodes:
-            node.delete_group_from_node(group)
-        print(f"Group {group} removed from all nodes")
+            print(node.groups)
+            for group in node.groups:
+                message = f"{node.url} - {group.groupID} - {all_groups.count(group)}"
+                if all_groups.count(group) != n_nodes:
+                    node.remove_group(group)
+                    print(message + "- deleted")
+                else:
+                    print(message + "- kept")
+
+    def check_consistency(func: Callable) -> Callable:
+        """Decorator to check if cluster is consistent before performing an operation"""
+        def wrapper(*args, **kwargs):
+            if args[0].validate_consistency():
+                return func(*args, **kwargs)
+            else:
+                args[0].rollback()
+                raise Exception("Cluster is not consistent, rollback performed")
+        return wrapper
+
+    @check_consistency
+    def add_node(self, node: Node) -> None:
+        if node not in self.nodes:
+            self.nodes.append(node)
+        else:
+            raise Exception("Node already exists")
+
+    @check_consistency
+    def remove_node(self, node: Node) -> None:
+        if node in self.nodes:
+            self.nodes.remove(node)
+        else:
+            raise Exception("Node does not exist")
+
+    @check_consistency
+    def add_group(self, group: Group):
+        for node in self.nodes:
+                node.add_group(group)
+        print(f"Group {group.groupID} added to all nodes")
         return group
 
-    def add_group_to_cluster(self, group: Group):
+    @check_consistency
+    def remove_group(self, group: Group):
         for node in self.nodes:
-            node.add_group_to_node(group)
-        print(f"Group {group.groupID} added to all nodes")
+            node.remove_group(group)
+        print(f"Group {group} removed from all nodes")
         return group
 
 
@@ -55,16 +108,3 @@ def find_group(cluster: Cluster, group_id: str) -> Group:
         if group.groupID == group_id:
             return group
     raise Exception(f"Group {group_id} not found in cluster")
-
-cluster = Cluster()
-
-HOSTS = [
-    'node1.example.com',
-    'node2.example.com',
-    'node3.example.com',
-]
-for host in HOSTS:
-    cluster.add_node(url=host)
-
-cluster.add_group_to_cluster(Group(groupID='group1'))
-cluster.add_group_to_cluster(Group(groupID='group2'))
