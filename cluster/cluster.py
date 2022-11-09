c3e0428162ab3702d1f17913b2f1bc599d8b6861
Eduardo Würch <ewurch@gmail.com>
2022-11-09 09:40:00 -0300

    add groups list to Node class

--- cluster/cluster.py
+++ cluster/cluster.py
@@ -1,7 +1,11 @@
 from pydantic import BaseModel
 
+class Group(BaseModel):
+    groupID: str
+
 class Node(BaseModel):
     url: str
+    groups: list[Group]
 
 class Cluster(BaseModel):
     nodes: list[Node]
